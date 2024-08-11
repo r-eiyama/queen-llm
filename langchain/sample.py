fc6c5339ca3dcd3fd7403bb57989641d1f92f621
r-eiyama <r-eiyama@macloud.jp>
2024-08-11 16:35:13 +0900

    azure open aiの呼び出し

--- langchain/sample.py
+++ langchain/sample.py
@@ -1,1 +1,18 @@
-print('hello world')
+from openai import AzureOpenAI
+import config
+
+# ２　APIクライアントの設定
+client = AzureOpenAI(
+    api_key = config.AZURE_OPENAI_API_KEY,
+    api_version = '2023-05-15', # this might change in the future
+    azure_endpoint = config.AZURE_OPENAI_ENDPOINT, # your endpoint should look like the following 
+)
+# ３　API呼出方法
+response = client.chat.completions.create(
+    model="gpt-4o",
+    messages=[
+        {"role": "user", "content": "日本で一番高い山を教えて下さい。"},
+    ],
+)
+# ４　レスポンスの処理
+print(response.choices[0].message.content)
